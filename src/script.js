d677f6d0027b33aa620bc3a5b57f8a1dd2a80b99
Dario849 <dariorodriguez7810@gmail.com>
2026-03-25 17:07:10 -0300

    V3.2.3

--- src/script.js
+++ src/script.js
@@ -1,35 +1,44 @@
-    const printBtn = document.getElementById('btnPdf');
-    printBtn.addEventListener('click', function () {
-        print();
+const printBtn = document.getElementById('btnPdf');
+const AppGroLink = document.getElementById('OpenAppGro');
+AppGroLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    Toastify({
+        text: 'El proyecto fue entregado, pero se dio de baja el hosting gratuito, por lo que no es posible acceder a él. Sin embargo, el código fuente del proyecto está disponible en mi repositorio de GitHub.',
+        duration: 5000,
+        close: true,
+    }).showToast();
+});
+printBtn.addEventListener('click', function () {
+    print();
+});
+if (window.localStorage.getItem('darkMode')) {
+    document.getElementById('themeCheckbox').checked = window.localStorage.getItem('darkMode') === 'true' ? true : false;
+    window.localStorage.getItem('darkMode') === 'true' ? toggleNightMode() : null;
+} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.getElementById('themeCheckbox').checked = true;
+    toggleNightMode();
+}
+function toggleNightMode() {
+    var checkbox = document.getElementById('themeCheckbox');
+    window.localStorage.setItem('darkMode', checkbox.checked);
+    const elements = document.querySelectorAll('body, .main, .aside, .footer, .contactItem, .footerPdf, .footerLink, .header');
+    elements.forEach(element => {
+        element.classList.toggle('darkMode');
     });
-    if(window.localStorage.getItem('darkMode')) {
-        document.getElementById('themeCheckbox').checked = window.localStorage.getItem('darkMode') ==='true'? true : false;
-        window.localStorage.getItem('darkMode') === 'true' ? toggleNightMode() : null;
-    }else if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.getElementById('themeCheckbox').checked = true;
-        toggleNightMode();
-    }
-    function toggleNightMode() {
-        var checkbox = document.getElementById('themeCheckbox');
-        window.localStorage.setItem('darkMode', checkbox.checked);
-        const elements = document.querySelectorAll('body, .main, .aside, .footer, .contactItem, .footerPdf, .footerLink, .header');
-        elements.forEach(element => {
-            element.classList.toggle('darkMode');
-        });
-    }
-    function copyToClipboard(text) {
-        navigator.clipboard.writeText(text).then(function () {
-            console.log('Email copiado al portapapeles: ' + text);
-        }, function (err) {
-            console.log('Error al copiar el email: ', err);
-        });
-    }
-    var qrcode = new QRCode(document.getElementById("qrcode"), {
-        text: "https://dario849.github.io/github-portfolio/",
-        width: 128,
-        height: 128,
-        colorDark: "#000000",
-        colorLight: "#ffffff",
-        correctLevel: QRCode.CorrectLevel.H
+}
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Email copiado al portapapeles: ' + text);
+    }, function (err) {
+        console.log('Error al copiar el email: ', err);
     });
-    document.getElementById("qrcode").title = "Podes escanear este QR para ver la versión web \n desde cualquier dispositivo movil";
+}
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+    text: "https://dario849.github.io/github-portfolio/",
+    width: 128,
+    height: 128,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H
+});
+document.getElementById("qrcode").title = "Podes escanear este QR para ver la versión web \n desde cualquier dispositivo movil";
